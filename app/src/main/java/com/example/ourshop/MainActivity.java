e28d5c084784128c33794681d535235c455cf49b
Rahmat Darmawan <rahmatdarmawan8899@gmail.com>
2021-10-05 22:05:04 +0700

    update the activity_main.xml and header_main.xml

--- app/src/main/java/com/example/ourshop/MainActivity.java
+++ app/src/main/java/com/example/ourshop/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.ourshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+
+        TextView textViewDate = findViewById(R.id.tvDate);
+        textViewDate.setText(currentDate);
     }
 }
