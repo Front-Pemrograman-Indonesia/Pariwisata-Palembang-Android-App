ffaad7f8585fdca9cac74ac7ca5467c9d5b4df85
onkkyalexander <onkkya26@gmail.com>
2021-11-01 22:21:02 +0700

    update vid splash_screen.xml

--- app/src/main/java/com/example/ourshop/activities/SplashScreen.java
+++ app/src/main/java/com/example/ourshop/activities/SplashScreen.java
@@ -1,24 +1,9 @@
 package com.example.ourshop.activities;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.VideoView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.ourshop.R;
 
 public class SplashScreen extends AppCompatActivity {
