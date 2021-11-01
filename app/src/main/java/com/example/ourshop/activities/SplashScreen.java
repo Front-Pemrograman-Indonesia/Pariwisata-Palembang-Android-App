13e80da95c4b1e3f74ab045808872b346c61f8aa
onkkyalexander <onkkya26@gmail.com>
2021-11-01 22:18:47 +0700

    update vid splash_screen.xml

--- app/src/main/java/com/example/ourshop/activities/SplashScreen.java
+++ app/src/main/java/com/example/ourshop/activities/SplashScreen.java
@@ -12,45 +12,34 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.ourshop.R;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private final int SPLASH_DISPLAY_LENGTH = 4300;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try {
-            VideoView videoHolder = new VideoView(this);
-            setContentView(videoHolder);
-            Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.vid_splashscreen);
-            videoHolder.setVideoURI(video);
+        setContentView(R.layout.activity_splash_screen);
 
-            videoHolder.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                public void onCompletion(MediaPlayer mp) {
-                    jump();
-                }
-            });
-            videoHolder.start();
-        } catch (Exception ex) {
-            jump();
-        }
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        jump();
-        return true;
-    }
-
-    private void jump() {
-        if (isFinishing())
-            return;
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
-    }
-
-    }
+}
