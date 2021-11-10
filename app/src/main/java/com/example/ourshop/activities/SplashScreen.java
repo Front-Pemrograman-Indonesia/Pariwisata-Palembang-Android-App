b8a31bbe1b7cec06465facd57a2a9a986b739bc5
onkkyalexander <onkkya26@gmail.com>
2021-11-10 18:28:57 +0700

    update vid splash_screen.xml

--- app/src/main/java/com/example/ourshop/activities/SplashScreen.java
+++ app/src/main/java/com/example/ourshop/activities/SplashScreen.java
@@ -1,60 +1,30 @@
 package com.example.ourshop.activities;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.ourshop.R;
 
+@SuppressLint("CustomSplashScreen")
+@SuppressWarnings("FieldCanBeLocal")
 public class SplashScreen extends AppCompatActivity {
+
+    private final int splash_display_length = 4300;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            getWindow().getDecorView().setSystemUiVisibility
-                    (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
-
-        if (Build.VERSION.SDK_INT >= 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-        }
-
-        //menghilangkan ActionBar
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_screen);
 
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                finish();
-            }
-        }, 3000L); //3000 L = 3 detik
-    }
-
-    //set Transparent Status bar
-    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
-
-        Window win = activity.getWindow();
-        WindowManager.LayoutParams winParams = win.getAttributes();
-        if (on) {
-            winParams.flags |= bits;
-        } else {
-            winParams.flags &= ~bits;
-        }
-        win.setAttributes(winParams);
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(() -> {
+            /* Create an Intent that will start the Menu-Activity. */
+            Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
+            SplashScreen.this.startActivity(mainIntent);
+            SplashScreen.this.finish();
+        }, splash_display_length);
     }
 }
