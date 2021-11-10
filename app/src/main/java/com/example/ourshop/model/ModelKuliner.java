8adac7f2b6ce245cfe1b746e11258db87b52d9b0
Satria Aluh Perwira Nusa <satrianusa10@gmail.com>
2021-11-11 00:15:04 +0700

    fix the main home screen

--- app/src/main/java/com/example/ourshop/model/ModelKuliner.java
+++ app/src/main/java/com/example/ourshop/model/ModelKuliner.java
@@ -2,67 +2,55 @@ package com.example.ourshop.model;
 
 import java.io.Serializable;
 
-/**
- * Created by Azhar Rivaldi on 22-12-2019.
- */
-
 public class ModelKuliner implements Serializable {
 
     private String idKuliner, txtNamaKuliner, txtAlamatKuliner, txtOpenTime, Koordinat, GambarKuliner, KategoriKuliner;
 
+    // Data Id Kuliner
+    public void setIdKuliner(String idKuliner) {
+        this.idKuliner = idKuliner;
+    }
     public String getIdKuliner() {
         return idKuliner;
     }
 
-    public void setIdKuliner(String idKuliner) {
-        this.idKuliner = idKuliner;
+    // Data Nama Kuliner
+    public void setTxtNamaKuliner(String txtNamaKuliner) {
+        this.txtNamaKuliner = txtNamaKuliner;
     }
-
     public String getTxtNamaKuliner() {
         return txtNamaKuliner;
     }
 
-    public void setTxtNamaKuliner(String txtNamaKuliner) {
-        this.txtNamaKuliner = txtNamaKuliner;
+    // Data Alamat Kuliner
+    public void setTxtAlamatKuliner(String txtAlamatKuliner) {
+        this.txtAlamatKuliner = txtAlamatKuliner;
     }
-
     public String getTxtAlamatKuliner() {
         return txtAlamatKuliner;
     }
 
-    public void setTxtAlamatKuliner(String txtAlamatKuliner) {
-        this.txtAlamatKuliner = txtAlamatKuliner;
+    // Data Waktu Buka
+    public void setTxtOpenTime(String txtOpenTime) {
+        this.txtOpenTime = txtOpenTime;
     }
-
     public String getTxtOpenTime() {
         return txtOpenTime;
     }
 
-    public void setTxtOpenTime(String txtOpenTime) {
-        this.txtOpenTime = txtOpenTime;
+    // Data Koordinat
+    public void setKoordinat(String koordinat) {
+        this.Koordinat = koordinat;
     }
-
     public String getKoordinat() {
         return Koordinat;
     }
 
-    public void setKoordinat(String koordinat) {
-        this.Koordinat = koordinat;
-    }
-
-    public String getGambarKuliner() {
-        return GambarKuliner;
-    }
-
+    // Data Thumbnail
     public void setGambarKuliner(String gambarKuliner) {
         this.GambarKuliner = gambarKuliner;
     }
-
-    public String getKategoriKuliner() {
-        return KategoriKuliner;
-    }
-
-    public void setKategoriKuliner(String kategoriKuliner) {
-        this.KategoriKuliner = kategoriKuliner;
+    public String getGambarKuliner() {
+        return GambarKuliner;
     }
 }
